f1f24d98bafd819a0f7fa84ca0687f2a69cd91c5
Hubert van Dongen <hmd19570@gmail.com>
2020-08-09 13:36:10 +0200

    Na video 10

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -33,4 +33,15 @@ class ParticipateInForum extends TestCase
         $this->get($thread->path())
             ->assertSee($reply->body);
     }
+
+    public function test_a_reply_requires_a_body()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path()  .  '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+    }
 }
