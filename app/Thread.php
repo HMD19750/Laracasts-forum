906cbe1463f0f919e0324bd79eb96a0557d26498
Hubert van Dongen <hmd19570@gmail.com>
2020-08-10 20:44:37 +0200

    Na video 16

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,15 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
     /**
      * Return value: path of the thread
      */
@@ -21,20 +30,24 @@ class Thread extends Model
         return $this->hasMany(Reply::class);
     }
 
+
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
 
     public function channel()
     {
         return $this->belongsTo(Channel::class);
     }
 
+
     public function addReply($reply)
     {
         $this->replies()->create($reply);
     }
+
 
     public function scopeFilter($query, $filters)
     {
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row ">
         <div class="col-md-8">
+
             <div class="card">
                 <div class="card-header">
                     <a href="#">
@@ -16,31 +17,46 @@
                     {{$thread->body}}
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @foreach($thread->replies as $reply)
-            @include('threads.reply')
+            @foreach($replies as $reply)
+                @include('threads.reply')
             @endforeach
-        </div>
-    </div>
-    @if(auth()->check())
-        <div class="row justify-content-center">
-            <div class="col-md-8 mt-5">
+
+            {{ $replies->links() }}
+
+            @if(auth()->check())
+
+            <div class=" mt-5">
                 <form method="POST" action="{{ $thread->path() . '/replies' }}">
                     @csrf
                     <div class="form-group">
-                        <textarea name="body" id="body" class="form-control" rows="5" placeholder="Have something to say?"></textarea>
+                        <textarea name="body" id="body" class="form-control" rows="5"
+                            placeholder="Have something to say?"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Post</button>
                 </form>
+
+            </div>
+            @else
+            <p class="text-center mt-5">Please
+                <a href="{{ route('login') }}">sign in</a> to participate in this discussion!</p>
+            @endif
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+
+                <div class="card-body">
+                    <p>This thread was published {{ $thread->created_at->diffForHumans() }}
+                        by
+                        <a href="#">{{ $thread->creator->name }}</a>
+                        and has {{ $thread->replies_count }} {{ Str::plural('comment')}}.
+                    </p>
+                </div>
             </div>
         </div>
-    @else
-    <p class="text-center mt-5">Please
-        <a href="{{ route('login') }}">sign in</a> to participate in this discussion!</p>
-    @endif
+
+    </div>
 </div>
+
 @endsection
