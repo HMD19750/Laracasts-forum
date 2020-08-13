1415315049bff9f195b60b976bf9a91e520a3ce5
Hubert van Dongen <hmd19570@gmail.com>
2020-08-13 21:48:21 +0200

    Na video 19

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,12 +1,30 @@
 <div class="card mt-2">
+
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a>
-             said {{ $reply->created_at->diffForHumans() }} ...
+
+        <div class="level">
+
+            <h5 class="flex">
+                <a href="#" >
+                {{ $reply->owner->name }}
+            </a>
+            said {{ $reply->created_at->diffForHumans() }} ...
+            </h5>
+
+            <div>
+                    <form method="POST" Action="{{url('replies')}}/{{ $reply->id }}/favorites" >
+                    @csrf
+                    <button type='submit' class='btn btn-link' {{ $reply->isFavorited()? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ Str::plural('Favorite',$reply->favorites()->count() ) }}
+                    </button>
+                </form>
+            </div>
+        </div>
 
     </div>
+
     <div class="card-body">
         {{$reply->body}}
     </div>
+
 </div>
